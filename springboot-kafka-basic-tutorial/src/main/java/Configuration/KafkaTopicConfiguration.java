cd02048487044ce28ff62b5112082fe4bb7439b1
Ahmer Hasnain <60224683+ahmerhasnain@users.noreply.github.com>
2024-11-05 18:34:36 +0500

    Add files via upload

---
+++ springboot-kafka-basic-tutorial/src/main/java/Configuration/KafkaTopicConfiguration.java
@@ -0,0 +1,14 @@
+package Configuration;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfiguration {
+    @Bean
+    public NewTopic MyTopic() {
+        return TopicBuilder.name("Topic").build();
+    }
+}
---
+++ springboot-kafka-basic-tutorial/src/main/java/Kafka/KafkaProducer.java
@@ -0,0 +1,21 @@
+package Kafka;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class KafkaProducer {
+    private static  final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
+    private KafkaTemplate<String, String> kafkaTemplate;
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+    public void sendMessage(String message) {
+        logger.info("Sent message: " + message);
+        kafkaTemplate.send("Topic", message);
+    }
+}
